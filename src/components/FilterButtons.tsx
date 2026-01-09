f768751bca6666ea36e2a1f54b91314d89748b2a
Mag <166319982+magali-la@users.noreply.github.com>
2026-01-09 18:26:03 -0500

    feat(filters): import context, set handler for selecting a filter button, create conditional active styling to filter buttons

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -1,10 +1,25 @@
+import { useContext } from "react"
+import { FilterContext } from "../contexts/FilterContext"
+import type { Filters } from "../types";
+
 export default function FilterButtons() {
+    // non null value for ts error
+    const { filter, setFilter } = useContext(FilterContext)!;
+
+    // handle filter select
+    function handleFilterSelect(event: React.MouseEvent<HTMLButtonElement>) {
+        // use currenttarget for button precision and as keyword to ensure ts its correct
+        setFilter(event.currentTarget.value as Filters);
+    }
+
+    // conditional style for active status via ternary or default styling in index.css
+    const activeStyle = 'bg-amber-600! text-white';
 
     return (
         <div className="flex flex-row w-full justify-center gap-8">
-            <button className="bg-amber-600! text-white" value="All">All</button>
-            <button className="bg-orange-700" value="All">Active</button>
-            <button className="bg-orange-700" value="All">Completed</button>
+            <button className={filter === 'all' ? activeStyle : ''} value="all" onClick={handleFilterSelect}>All</button>
+            <button className={filter === 'active' ? activeStyle : ''} value="active" onClick={handleFilterSelect}>Active</button>
+            <button className={filter === 'completed' ? activeStyle : ''} value="completed" onClick={handleFilterSelect}>Completed</button>
         </div>
     )
 }
